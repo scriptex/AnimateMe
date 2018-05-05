279499fd035108d7731b0dd467e0ebec636dfc0c
Atanas Atanasov <scriptex.bg@gmail.com>
2018-05-05 07:33:08 +0300

    Re-build dist files

--- dist/animate.me.js
+++ dist/animate.me.js
@@ -91,6 +91,10 @@ Object.defineProperty(exports, "__esModule", {
 });
 exports.default = void 0;
 
+function _objectSpread(target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i] != null ? arguments[i] : {}; var ownKeys = Object.keys(source); if (typeof Object.getOwnPropertySymbols === 'function') { ownKeys = ownKeys.concat(Object.getOwnPropertySymbols(source).filter(function (sym) { return Object.getOwnPropertyDescriptor(source, sym).enumerable; })); } ownKeys.forEach(function (key) { _defineProperty(target, key, source[key]); }); } return target; }
+
+function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
+
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
 function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
@@ -106,7 +110,7 @@ function () {
 
     _classCallCheck(this, AnimateMe);
 
-    this.options = Object.assign({}, {
+    this.options = _objectSpread({
       offset: 0.5,
       reverse: true,
       animatedIn: 'animate-me--in',
